67dca4dc008ef27f962ec27ce3f0348b023a84c0
Matt Guthrie <matt@mkguthrie.com>
2019-07-08 20:04:08 -0700

    added portfolio and single portfolio pages

---
+++ portfolio.js
@@ -0,0 +1,142 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Work+Sans:300,400,500&display=swap" rel="stylesheet">
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <link rel="stylesheet" href="./portfolio.css">
+    <title>Matt Guthrie</title>
+  </head>
+  <body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light border-bottom">
+            <a class="navbar-brand" href="#">mkg</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse center-nav-for-desktop justify-content-end" id="navbarNavAltMarkup">
+              <div class="navbar-nav">
+                <a class="nav-item nav-link active px-3 border-right" href="#">Portfolio <span class="sr-only">(current)</span></a>
+                <a class="nav-item nav-link px-3 border-right" href="#">Blog</a>
+                <a class="nav-item nav-link px-3" href="#">Now</a>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <div class="jumbotron bg-white jumbotron-fluid">
+        <div class="container d-flex flex-column justify-content-center align-items-center">
+            <img src="./img/mkg_headshot.jpg" alt="headshot of mkg" class="rounded-circle mg-fluid img-thumbnail" width="175px" height="175px" >
+            <h1 class="display-4 pt-3">Matt Guthrie</h1>
+            <h3 class="text-muted">Front End Developer</h3>
+            <div class="container">
+                    <div class="row align-items-start">
+                      <div class="col">
+                        
+                      </div>
+                      <div class="col-8">
+                            <p class="lead pt-3">Duis aliquam erat est, ut volutpat urna commodo ullamcorper. Donec tempor, elit quis pellentesque convallis, tellus nunc fermentum enim, vel mollis est lorem id turpis. Duis aliquam erat est, ut volutpat urna commodo ullamcorper. Donec tempor, elit quis pellentesque convallis, tellus nunc fermentum enim, vel mollis est lorem id turpis.</p>
+                      </div>
+                      <div class="col">
+                       
+                      </div>
+                    </div>
+            
+            </div>
+        </div>
+    </div>
+    <div class="py-5">
+        <div class="container">
+            <h3 class="pb-4">Client Work</h3>
+            <div class="row">
+                <div class="col-lg-4 col-md-6">
+                  <div class="card mb-4 shadow-sm">
+                    <img src="./img/hemp_cc.jpg" alt="portfolio-image" class="border-bottom" width="100%" height="225">
+                        <div class="card-body">
+                            <h4 class="portfolio-title">HempCheebaChews</h4>
+                            <p class="portfolio-text">E-commerce WordPress Store</p>
+                            <div class="d-flex justify-content-between align-items-center">
+                                <div class="btn-group">
+                                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                                </div>
+                            </div>
+                        </div>
+                  </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="card mb-4 shadow-sm">
+                        <img src="./img/hemp_cc.jpg" alt="portfolio-image" class="border-bottom" width="100%" height="225">
+                            <div class="card-body">
+                                <h4 class="portfolio-title">HempCheebaChews</h4>
+                                <p class="portfolio-text">E-commerce WordPress Store</p>
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <div class="btn-group">
+                                        <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                                    </div>
+                                </div>
+                            </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="card mb-4 shadow-sm">
+                        <img src="./img/hemp_cc.jpg" alt="portfolio-image" class="border-bottom" width="100%" height="225">
+                            <div class="card-body">
+                                <h4 class="portfolio-title">HempCheebaChews</h4>
+                                <p class="portfolio-text">E-commerce WordPress Store</p>
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <div class="btn-group">
+                                        <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                                    </div>
+                                </div>
+                            </div>
+                    </div>
+                </div>
+            </div>
+            <a class="" href="#">View Portfolio</a>
+        </div>
+    </div>
+    <div class="py-5">
+        <div class="container">
+            <h3 class="pb-4">Blog Posts</h3>
+            <div class="row">
+                <div class="col">
+                    <div class="mb-4">
+                        <div class="">
+                            <h4 class=""><a href="./single-post.html">How to Build a Custom WordPress Theme</a></h4>
+                            <p class="">My experience building a custom theme</p>
+                        </div>
+                        <div class="">
+                            <h4 class="">How to Build a Custom WordPress Theme</h4>
+                            <p class="">My experience building a custom theme</p>
+                        </div>
+                        <div class="">
+                            <h4 class="">How to Build a Custom WordPress Theme</h4>
+                            <p class="">My experience building a custom theme</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <a class="" href="#">View Portfolio</a>
+        </div>
+    </div>
+    <footer class="py-3 bg-white border-top">
+        <div class="container">
+            <i class="fa fa-github-square fa-3x pr-2"></i>
+            <i class="fa fa-twitter-square fa-3x pr-2"></i>
+            <i class="fa fa-envelope-square fa-3x"></i>
+        </div>
+    </footer>
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="./portfolio.js"></script>
+  </body>
+</html>
